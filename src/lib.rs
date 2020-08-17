505d211161c66f5d78f13447a1ded6dd9c857277
Andy Bell <andy.bell@displaynote.com>
2020-08-17 13:17:31 +0200

    another hero using generics

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,30 @@ impl Hero1 {
     }
 }
 
+use std::cmp::Eq;
+use std::hash::Hash;
+use num::traits::{Zero};
+
+pub struct Hero2<P, V> {
+    props: HashMap<P, V>
+}
+
+impl<P: Hash + Eq, V: Zero + Copy> Hero2<P, V> {
+    pub fn new() -> Self {
+        Self {
+            props: HashMap::new()
+        }
+    }
+
+    pub fn set_prop(&mut self, prop: P, value: V) {
+        self.props.insert(prop, value);
+    }
+
+    pub fn get_prop(&self, prop: P) -> V {
+        *self.props.get(&prop).unwrap_or(&V::zero())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -57,6 +81,13 @@ mod tests {
     #[test]
     fn second_hero() {
         let mut hero = Hero1::new();
+        hero.set_prop(1, 100);
+        assert_eq!(100, hero.get_prop(1));
+    }
+
+    #[test]
+    fn third_hero() {
+        let mut hero = Hero2::<u8, u32>::new();
         hero.set_prop(1, 100);
         assert_eq!(100, hero.get_prop(1));
     }
